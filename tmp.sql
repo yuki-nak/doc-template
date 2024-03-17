ddf28ae95dfcbe42e82818c4d0e20e0a24b0c405
yuki-nak <33897862+yuki-nak@users.noreply.github.com>
2024-03-17 20:03:02 +0900

    Delete tmp.sql

--- tmp.sql
+++
@@ -1,7 +0,0 @@
-SELECT f.filename
-FROM files AS f
-INNER JOIN (
-    SELECT LEFT(filename, INSTR(filename, '-') - 1) AS prefix, MAX(MID(filename, INSTR(filename, '-') + 1, INSTR(filename, '.') - INSTR(filename, '-') - 1)) AS max_number
-    FROM files
-    GROUP BY LEFT(filename, INSTR(filename, '-') - 1)
-) AS subquery ON LEFT(f.filename, INSTR(f.filename, '-') - 1) = subquery.prefix AND MID(f.filename, INSTR(f.filename, '-') + 1, INSTR(f.filename, '.') - INSTR(f.filename, '-') - 1) = subquery.max_number;
